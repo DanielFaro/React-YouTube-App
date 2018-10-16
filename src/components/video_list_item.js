a9123117a4184e2fd477a6f99c19af647cbc7555
DanielFaro <Danieln.Faro@gmail.com>
2018-10-16 19:18:16 -0400

    update1

---
+++ src/components/video_list_item.js
@@ -0,0 +1,21 @@
+import '../index.css';
+import React from 'react';
+
+const VideoListItem = ({ video, onVideoSelect }) => {
+    const imageUrl = video.thumbnails.default.url;
+
+    return (
+        <li onClick={() => onVideoSelect(video)} className="list-group-item">
+            <div className="video-list media">
+                <div className="media-left">
+                    <img className="media-object" src={imageUrl} alt="BlankImage"/>
+                </div>
+                <div className="media-body">
+                    <div className="media-heading">{video.title}</div>
+                </div>
+            </div>   
+        </li>
+    );
+};
+
+export default VideoListItem;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,59 @@
-import React from 'react';
+import _ from 'lodash';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import youtubeSearch from 'youtube-search';
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+// const API_KEY = 'AIzaSyChAIt5WK61KboIBxaYiMNiXLu7LaK4A2k'; //api key for youtube
+const API_KEY = 'AIzaSyDdKNB63q_HqhL0HBWZRfhgn-C_SzY7E8M';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class App extends Component {
+    constructor(props) {
+        super(props);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+        this.state = {
+            videos: [],
+            selectedVideo: null
+        };
+
+        this.videoSearch('surfboards');
+
+    }
+
+    videoSearch(term) {
+        console.log(term);
+        youtubeSearch(term, { key: API_KEY }, (err, videos) => { //videos can be called anything
+            if (err) return console.log(err);
+            console.dir(videos);
+            this.setState({
+                videos: videos,
+                selectedVideo: videos[0]
+            });
+        });
+    }
+
+    render() {
+        const videoSearch = _.debounce((term) => { this.videoSearch(term) }, 300);
+        console.log(this.state.videos);
+        console.log(this.state.selectedVideo)
+        return (
+            <div>
+                {this.state.videos !== [] &&
+                    <div>
+                        <SearchBar onSearchTermChange={videoSearch} />
+                        <VideoDetail video={this.state.selectedVideo} />
+                        <VideoList
+                            onVideoSelect={selectedVideo => this.setState({ selectedVideo })}//updates the app state with the newest selected video
+                            videos={this.state.videos} />
+                    </div>
+                 }
+            </div>
+        );
+    }
+}
+
+//take this component's generated HTML and put
+//it on the page (DOM)
+ReactDOM.render(<App />, document.querySelector('#root')); // possibly container not root
